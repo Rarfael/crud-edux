ec9a095570c65cddf869651d472e43fc2eaccee2
Rafael Godoy <rarfaelgoodoy@gmail.com>
2019-04-13 13:20:26 -0300

    Created Empresa model and migration and removed laravel users default model and migration

---
+++ database/migrations/2019_04_12_203341_create_empresas_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('CNPJ', 14)->unique();
+            $table->string('razao_social');
+            $table->string('nome_fantasia');
+            $table->integer('CEP');
+            $table->integer('logradouro');
+            $table->text('numero');
+            $table->text('telefone', 11);
+            $table->string('email');
+            $table->text('complemento')->nullable();
+            $table->text('bairro');
+            $table->text('cidade');
+            $table->string('estado', 2);
+            $table->string('segmento');
+            $table->text('inscricao_municipal');
+            $table->text('incricao_estadual')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
