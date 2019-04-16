a712a79f09360072c1f660a04d824fcf6e81e2f6
Rafael Godoy <rarfaelgoodoy@gmail.com>
2019-04-16 17:03:48 -0300

    Making the create form a Vue component

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,258 @@
+<template>
+  <div>
+    <h2>Empresa</h2>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Nome Fantasia *</label>
+          <input
+            type="text"
+            name="nome_fantasia"
+            v-model="nome_fantasia"
+            class="form-control"
+            placeholder="Nomda da Empresa"
+            required="required"
+          >
+          <div v-if="error" class="alert alert-danger">{{ error }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>CNPJ *</label>
+          <input
+            type="text"
+            name="CNPJ"
+            class="form-control"
+            v-model="cnpj"
+            placeholder="Ex: 12345678912345"
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Razão Social *</label>
+          <input
+            type="text"
+            name="razao_social"
+            v-model="razao_social"
+            class="form-control"
+            placeholder="Razão Social"
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Segmento</label>
+          <select name="segmento" class="form-control" v-model="segmento">
+            <option>Produto</option>
+            <option>Serviço</option>
+            <option>Produto e Serviço</option>
+          </select>
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Inscrição Estadual *</label>
+          <input
+            type="text"
+            name="inscricao_estadual"
+            v-model="inscricao_estadual"
+            class="form-control"
+            placeholder="Inscrição estadual *"
+            required="required"
+            data-error="Valid email is required."
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Inscrição Municipal</label>
+          <input
+            type="text"
+            name="inscricao_municipal"
+            v-model="inscricao_municipal"
+            class="form-control"
+            placeholder="Inscrição municipal"
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+    </div>
+
+    <h3>Contato</h3>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label>Telefone *</label>
+          <input
+            type="tel"
+            name="telefone"
+            v-model="telefone"
+            class="form-control"
+            placeholder="Por favor escrava o numero de telefone"
+            required="required"
+            data-error="name is required."
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <label>Email *</label>
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            class="form-control"
+            placeholder="Por favor digite o e-mail"
+            required="required"
+            data-error="name is required."
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+    </div>
+
+    <h3>Endereço</h3>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>CEP *</label>
+          <input
+            type="number"
+            name="CEP"
+            v-model="cep"
+            class="form-control"
+            placeholder="Por favor digite o cep"
+            required="required"
+            data-error="name is required."
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Cidade *</label>
+          <input
+            type="text"
+            name="cidade"
+            v-model="cidade"
+            class="form-control"
+            placeholder="Por favor informe a cidade *"
+            required="required"
+            data-error="Valid email is required."
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Estado *</label>
+          <input
+            type="text"
+            name="estado"
+            v-model="estado"
+            class="form-control"
+            placeholder="Por favor informe o estado"
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Logradouro *</label>
+          <input
+            type="text"
+            name="logradouro"
+            v-model="logradouro"
+            class="form-control"
+            placeholder="Logradouro"
+            required="required"
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Bairro *</label>
+          <input
+            type="text"
+            name="bairro"
+            v-model="bairro"
+            class="form-control"
+            placeholder="Bairro"
+            required="required"
+            data-error="Valid email is required."
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Complemento *</label>
+          <input
+            type="text"
+            name="complemento"
+            v-model="complemento"
+            class="form-control"
+            placeholder="Complemento"
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="form-group">
+          <label>Numero *</label>
+          <input
+            type="text"
+            name="numero"
+            v-model="numero"
+            class="form-control"
+            placeholder="numero"
+            required="required"
+            data-error="name is required."
+          >
+          <div class="alert alert-danger">{{ }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "cnpj",
+    "razao_social",
+    "nome_fantasia",
+    "cep",
+    "logradouro",
+    "numero",
+    "telefone",
+    "email",
+    "complemento",
+    "bairro",
+    "cidade",
+    "estado",
+    "segmento",
+    "inscricao_municipal",
+    "inscricao_estadual",
+    "error"
+  ],
+  mounted() {
+    console.log(console.log(this.errors));
+  }
+};
+</script>
--- resources/views/empresa/includes/form.blade.php
+++ resources/views/empresa/includes/form.blade.php
@@ -10,134 +10,23 @@
     <form method="post"  action="{{ !empty($empresa) ? route('update', $empresa->id) : route('store') }}" 
         enctype="multipart/form-data">
         @csrf
-        <h2>Empresa</h2>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Nome Fantasia *</label>
-                    <input type="text" name="nome_fantasia" value="{{ old('nome_fantasia') ?? $empresa->nome_fantasia ?? null }}" class="form-control" placeholder="Nomda da Empresa" required="required" >
-                    @if ($errors->has('nome_fantasia'))<div class="alert alert-danger">{{ $errors->first('nome_fantasia') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>CNPJ *</label>
-                    <input type="text" name="CNPJ" class="form-control" value="{{ old('CNPJ') ?? $empresa->CNPJ ?? null }}" placeholder="Ex: 12345678912345" data-error="Valid email is required.">
-                    @if ($errors->has('CNPJ'))<div class="alert alert-danger">{{ $errors->first('CNPJ') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Razão Social *</label>
-                    <input type="text" name="razao_social" value="{{ old('razao_social') ?? $empresa->razao_social ?? null }}" class="form-control" placeholder="Razão Social">
-                    @if ($errors->has('razao_social'))<div class="alert alert-danger">{{ $errors->first('razao_social') }}</div> @endif
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                <label >Segmento</label>
-                  <select name="segmento" class="form-control" value="{{ old('segmento') ?? $empresa->segmento ?? null }}" >
-                    <option>Produto</option>
-                    <option>Serviço</option>
-                    <option>Produto e Serviço</option>
-                  </select>
-                  @if ($errors->has('segmento'))<div class="alert alert-danger">{{ $errors->first('segmento') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Inscrição Estadual *</label>
-                    <input type="text" name="inscricao_estadual" value="{{ old('inscricao_estadual') ?? $empresa->inscricao_estadual ?? null }}" class="form-control" placeholder="Inscrição estadual *" required="required" data-error="Valid email is required.">
-                    @if ($errors->has('inscricao_estadual'))<div class="alert alert-danger">{{ $errors->first('inscricao_estadual') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Inscrição Municipal </label>
-                    <input type="text" name="inscricao_municipal" value="{{ old('inscricao_municipal') ?? $empresa->inscricao_municipal ?? null }}" class="form-control" placeholder="Inscrição municipal">
-                    @if ($errors->has('inscricao_municipal'))<div class="alert alert-danger">{{ $errors->first('inscricao_municipal') }}</div> @endif
-                </div>
-            </div>
-        </div>
-
-        <h3>Contato</h3>
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <label>Telefone *</label>
-                    <input type="tel" name="telefone" value="{{ old('telefone') ?? $empresa->telefone ?? null }}" class="form-control" placeholder="Por favor escrava o numero de telefone" required="required" data-error="name is required.">
-                    @if ($errors->has('telefone'))<div class="alert alert-danger">{{ $errors->first('telefone') }}</div> @endif
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <label>Email *</label>
-                    <input type="email" name="email" value="{{ old('email') ?? $empresa->email ?? null }}" class="form-control" placeholder="Por favor digite o e-mail" required="required" data-error="name is required.">
-                    @if ($errors->has('email'))<div class="alert alert-danger">{{ $errors->first('email') }}</div> @endif
-                </div>
-            </div>
-        </div>
-
-        <h3>Endereço</h3>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>CEP *</label>
-                    <input type="number" name="CEP" value="{{ old('CEP') ?? $empresa->CEP ?? null }}" class="form-control" placeholder="Por favor digite o cep" required="required" data-error="name is required.">
-                    @if ($errors->has('CEP'))<div class="alert alert-danger">{{ $errors->first('CEP') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label >Cidade *</label>
-                    <input type="text" name="cidade" value="{{ old('cidade') ?? $empresa->cidade ?? null }}" class="form-control" placeholder="Por favor informe a cidade *" required="required" data-error="Valid email is required.">
-                    @if ($errors->has('cidade'))<div class="alert alert-danger">{{ $errors->first('cidade') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Estado *</label>
-                    <input type="text" name="estado" value="{{ old('estado') ?? $empresa->estado ?? null }}" class="form-control" placeholder="Por favor informe o estado">
-                    @if ($errors->has('estado'))<div class="alert alert-danger">{{ $errors->first('estado') }}</div> @endif
-                </div>
-            </div>
-        </div>
-                <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Logradouro *</label>
-                    <input type="text" name="logradouro" value="{{ old('logradouro') ?? $empresa->logradouro ?? null }}" class="form-control" placeholder="Logradouro" required="required">
-                    @if ($errors->has('logradouro'))<div class="alert alert-danger">{{ $errors->first('logradouro') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Bairro *</label>
-                    <input type="text" name="bairro" value="{{ old('bairro') ?? $empresa->bairro ?? null }}" class="form-control" placeholder="Bairro" required="required" data-error="Valid email is required.">
-                    @if ($errors->has('bairro'))<div class="alert alert-danger">{{ $errors->first('bairro') }}</div> @endif
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Complemento *</label>
-                    <input type="text" name="complemento" value="{{ old('complemento') ?? $empresa->complemento ?? null }}" class="form-control" placeholder="Complemento">
-                    @if ($errors->has('complemento'))<div class="alert alert-danger">{{ $errors->first('complemento') }}</div> @endif
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="form-group">
-                    <label>Numero *</label>
-                    <input type="text" name="numero" value="{{ old('numero') ?? $empresa->numero ?? null }}" class="form-control" placeholder="numero" required="required" data-error="name is required.">
-                    @if ($errors->has('numero'))<div class="alert alert-danger">{{ $errors->first('numero') }}</div> @endif
-                </div>
-            </div>
-        </div>
+            <form-component nome_fantasia="{{ old('nome_fantasia') ?? $empresa->nome_fantasia ?? null }}"
+                            cnpj = "{{ old('CNPJ') ?? $empresa->CNPJ ?? null }}"
+                            razao_social = "{{ old('razao_social') ?? $empresa->razao_social ?? null }}"
+                            cep = "{{ old('CEP') ?? $empresa->CEP ?? null }}"
+                            logradouro = "{{ old('logradouro') ?? $empresa->logradouro ?? null }}"
+                            numero = "{{ old('numero') ?? $empresa->numero ?? null }}"
+                            telefone = "{{ old('telefone') ?? $empresa->telefone ?? null }}"
+                            email = "{{ old('email') ?? $empresa->email ?? null }}"
+                            complemento = "{{ old('complemento') ?? $empresa->complemento ?? null }}"
+                            bairro = "{{ old('bairro') ?? $empresa->bairro ?? null }}"
+                            cidade = "{{ old('cidade') ?? $empresa->cidade ?? null }}"
+                            estado = "{{ old('estado') ?? $empresa->estado ?? null }}"
+                            segmento = "{{ old('segmento') ?? $empresa->segmento ?? null }}"
+                            inscricao_municipal = "{{ old('inscricao_municipal') ?? $empresa->inscricao_municipal ?? null }}"
+                            inscricao_estadual = "{{ old('inscricao_estadual') ?? $empresa->inscricao_estadual ?? null }}"
+                            erros = "{{ $errors }}"
+                            @if ($errors->has('nome_fantasia')) error="{{ $errors->first('nome_fantasia') }}" @endif></form-component>
 
         <div class="row">
             <div class="col-md-6">
