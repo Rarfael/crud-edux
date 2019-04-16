e2d885acf6d289d2c7762c9e83f5806ea22b980e
Rafael Godoy <rarfaelgoodoy@gmail.com>
2019-04-15 22:38:31 -0300

    Adjusted company info page

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,34 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+  <section class="row">
+    <div class="col-md-12 col-lg-6">
+      <div class="col-sm-12">
+        <div class="col-xs-12 col-sm-8">
+          <h2>{{ empresa.nome_fantasia }}</h2>
+          <p>
+            <strong>CNPJ:</strong>
+            {{ empresa.CNPJ }}
+          </p>
+          <p>
+            <strong>Razão Social:</strong>
+            {{ empresa.razao_social }}
+          </p>
+          <p>
+            <strong>CEP:</strong>
+            {{ empresa.CEP }}
+          </p>
         </div>
+      </div>
     </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  prop: {
+    empresa: ""
+  },
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
 </script>
--- resources/views/empresa/empresa.blade.php
+++ resources/views/empresa/empresa.blade.php
@@ -8,15 +8,21 @@
         <div class="col-sm-12">
             <div class="col-xs-12 col-sm-8">
                 <h2>{{ $empresa->nome_fantasia }}</h2>
+
                 <p><strong>CNPJ: </strong> {{ $empresa->CNPJ }} </p>
-                <p><strong>Razão Social: </strong> {{ $empresa->razao_social }}</p>
+                <p><strong>Razao Social: </strong> {{ $empresa->razao_social }} </p>
                 <p><strong>CEP: </strong> {{ $empresa->CEP }} </p>
-                <p><strong>Skills: </strong>
-                    <span class="tags">html5</span> 
-                    <span class="tags">css3</span>
-                    <span class="tags">jquery</span>
-                    <span class="tags">bootstrap3</span>
-                </p>
+                <p><strong>Logradouro: </strong> {{ $empresa->logradouro }} </p>
+                <p><strong>Numero: </strong> {{ $empresa->numero }} </p>
+                <p><strong>Telefone: </strong> {{ $empresa->telefone }} </p>
+                <p><strong>Email: </strong> {{ $empresa->email }} </p>
+                <p><strong>Complemento: </strong> {{ $empresa->complemento }} </p>
+                <p><strong>Bairro: </strong> {{ $empresa->bairro }} </p>
+                <p><strong>Cidade: </strong> {{ $empresa->cidade }} </p>
+                <p><strong>Estado: </strong> {{ $empresa->estado }} </p>
+                <p><strong>Segmento: </strong> {{ $empresa->segmento }} </p>
+                <p><strong>Inscricao Municipal: </strong> {{ $empresa->inscricao_municipal }} </p>
+                <p><strong>Inscricao Estadual: </strong> {{ $empresa->inscricao_estadual }} </p>
             </div>
         </div>
 	</div>
